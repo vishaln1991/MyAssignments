8429b4d56e608786c90c8c7f4dc02d4b0f68e162
vishaln1991 <67590609+vishaln1991@users.noreply.github.com>
2024-04-27 20:41:48 +0530

    day 1 files

---
+++ wek1/day1/Firefox.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+		
+		float browserVersion = 100.2f;
+		String browserName = "firefox";
+		boolean isVisible = true;
+		int releaseYear = 1998;
+        char browserLogo = 'f';
+        
+        System.out.println("The browser version is: " +browserVersion);
+        System.out.println("The browser Name is: " +browserName);
+        System.out.println("Check if browser is visible: "+isVisible);
+        System.out.println("Release Year is: " +1998);
+        System.out.println("Logo Browser : " +browserLogo);
+	}
+
+}
---
+++ wek1/day1/FirstProgram.java
@@ -0,0 +1,27 @@
+package week1.day1;
+
+public class FirstProgram {
+
+	// create a main method
+	// entry point for execution
+	
+	public static void main(String[] args) {
+		
+		//print a statement
+		
+		// datatype variablename = value
+		int age = 30; 
+		long phoneNumber = 9876543210L;
+		float height = 5.243434f;
+		double bankBalance = 10007.8988;
+		char logo = 'H';
+		
+		String registrationNumber = "TN000078";
+		boolean isMarried = true;
+		System.out.println("The age is: " +age);
+		System.out.println(phoneNumber);
+		System.out.println("Welcome to TestLeaf");
+		
+	}
+	
+}
